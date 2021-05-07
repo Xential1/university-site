22e97fa07fd0a15b6b2b1d5ca7c2a6c45dcb1a2e
stephen <stephenchinwuba060@gmail.com>
2021-05-07 15:40:59 +0100

    new changes

--- form-handle.php
+++ form-handle.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = '';
+$email_from = 'https://9jauniversity.netlify.app';
 
 $email_subject = 'New Form Submission';
 
